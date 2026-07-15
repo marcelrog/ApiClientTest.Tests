d5b78f33904d425e480d1741336a0d3f7d5bf39e
MarcelRogTestCrewIT <marcelrog89@gmail.com>
2026-07-15 09:03:36 +0200

    Add HTML reporter to Playwright config

--- playwright.config.ts
+++ playwright.config.ts
@@ -1,9 +1,9 @@
 import { defineConfig } from '@playwright/test';
 
 export default defineConfig({
-testDir: './ui',
+  reporter: [['html', { outputFolder: 'playwright-report', open: 'never' }]],
   use: {
-    headless: true,
-    viewport: { width: 1280, height: 720 },
+    // Dit is de fix
+    testIdAttribute: 'data-testid'
   },
 });
